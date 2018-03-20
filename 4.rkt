0e06ad580a9faecd8cfc760eb36909f393bc78d6
Patrick Olila <37233640+polila@users.noreply.github.com>
2018-03-20 03:44:41 -0400

    Update 4.rkt

--- 4.rkt
+++ 4.rkt
@@ -1,12 +1,11 @@
 #lang racket
 ; https://projecteuler.net/problem=4
 ; A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
 ; Find the largest palindrome made from the product of two 3-digit numbers.
-
 ; Answer = 906,609 (Completed in Java)
 ; Could not solve runtime issue with memory when .rkt file is executed
 ; posted question on StackOverflow: https://stackoverflow.com/questions/49376592/calculating-n-digit-palindromes-infinite-loop
+
 (define (exp a b)
     (define (iter product count)
       (if (= count b)
