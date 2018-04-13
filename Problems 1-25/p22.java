e9494ce464d5e3bb0cb1815b1adcdbefdd14bdd3
Patrick Olila <polila@ryerson.ca>
2018-04-13 18:28:23 -0400

    Problem 22, 23

--- Problems 1-25/p22.java
+++ Problems 1-25/p22.java
@@ -14,32 +14,110 @@ import java.util.*;
 import java.io.*;
 
 public class p22 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		int[] charValue = new int[('Z' - 'A' ) + 1];
+
+		Hashtable<String, Integer> letterValue = new Hashtable<String, Integer>();
+
+		String letter;
+
+		for (int i = 0, j = (int) 'A'; i < charValue.length; i++, j++) {
+
+			charValue[i] = j - 64;
+
+			letter = Character.toString((char) j);
+
+			letterValue.put(letter, charValue[i]);
+		}
 
 		String file = "p022_names.txt";
 
-		String line = null;
+		Scanner in = null;
 
-		String str = "";
+		int sum, position;
+		position = 1;
 
-		FileReader in;
+		String str, temp;
+
+		char ch;
+
+		Hashtable<String, Integer> nameValue = new Hashtable<String, Integer>();
+
+		Hashtable<String, Integer> namePosition = new Hashtable<String, Integer>();
+
+		Hashtable<Integer, String> nameIndex = new Hashtable<Integer, String>();
 
 		try {
 
-			in = new FileReader(file);
+			in = new Scanner(new BufferedReader(new FileReader(file)));
 
-			BufferedReader read = new BufferedReader(in);
+			while (in.useDelimiter(",").hasNext()) {
+					
+					str = in.next();
 
-			while ((line = read.readLine()) != null) {
-				str = str + line;
-				System.out.println(line);
+					temp = "";
+
+					sum = 0;
+
+					for (int i = 1; i < str.length() - 1; i++) {
+
+						ch = str.charAt(i);
+
+						temp = temp + Character.toString(ch);
+
+						sum = sum + letterValue.get(Character.toString(ch));
+					}
+
+					nameIndex.put(position,temp);
+
+					nameValue.put(temp,sum);
+
+					namePosition.put(temp,position);
+
+					position++; 
 			}
+		} finally {
 
-			read.close();
+			if (in != null) {
 
-		} catch (IOException e) {
-
-			e.printStackTrace();
+				in.close();
+			}
 		}
+
+		String[] arr = new String[position - 1];
+
+		String name;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			name = nameIndex.get(i + 1);
+
+			arr[i] = name;
+		}
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i].compareTo(arr[j]) > 0) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		sum = 0;
+
+		int product;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			product = nameValue.get(arr[i]) * (i + 1);
+
+			sum = sum + product;
+			
+		}
+
+		System.out.println("Total of all name score is: " + sum);
 	}
 }
---
+++ Problems 1-25/p23.java
@@ -0,0 +1,137 @@
+/*
+
+https://projecteuler.net/problem=23
+
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers.
+However, this upper limit cannot be reduced any further by analysis
+even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+*/
+
+import java.util.*;
+import java.math.*;
+
+public class p23 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static boolean isAbundant(int n) {
+		
+		return d(n) > n;
+	}
+
+	public static boolean isDeficient(int n) {
+		
+		return d(n) < n;
+	}
+
+	public static boolean isPerfect(int n) {
+
+		return d(n) == n;
+	}
+	// A function to compute the sum of proper divisors of a number n.
+	public static int d(int n) {
+		if (n == 0) {
+			return -1;
+		}
+
+		if (isPrime(n)) {
+			// Prime numbers have exactly one proper divisor, 1.
+			return 1;
+		}
+
+		int i, count, divisors, number;
+		i = 2;
+		number = n;
+
+		int sum = 1;
+
+		do {
+
+			divisors = 0;
+
+			if (n % i == 0) {
+
+				count = 0;
+
+				// Find prime factors.
+				while (n % i == 0) {
+
+					n = n / i;
+					// Count how many times factor can be raised.
+					count++;
+				}
+
+				int j, product;
+				j = 0;
+				product = 1;
+
+				while (j < count) {
+					
+					product = product * i;
+					// Sum the factor and itself raised to
+					// the possible powers it can be raised.
+					divisors = divisors + product;
+
+					j++;
+				}
+				// The sum of divisors can be expressed
+				// as the prodduct of the sum of the
+				// factors and the powers it can be raised;
+				// (1 + 1p^1 + 1p^2 + . . . + 1p^k) x (1 + 2p^1 + 2p^2 + . . . + 2pk)
+				sum = sum * (1 + divisors);
+			}
+
+			i++;
+
+		} while (i <= n);
+
+		// Subtract n since proper divisors
+		// are divisors other than the number itself.
+		sum = sum - number;
+
+		return sum;
+	}
+
+	public static boolean isPrime(int n) {
+
+		if (n <= 1) {
+			return false;
+		}
+
+		else if (n <= 3){
+			return true;
+		}
+
+		else if (n % 2 == 0 || n % 3 == 0) {
+			return false;
+		}
+
+		else {
+
+			int i = 5;
+
+			while (i * i <= n) {
+
+				if (n % i == 0 || n % (i + 2) == 0) {
+
+					return false;
+				}
+
+				i = i + 6;
+			}
+		}
+
+		return true;
+	}
+}
