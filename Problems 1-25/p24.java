863961e89c7060de885f8832344d0ed27d1dee3c
Patrick Olila <polila@ryerson.ca>
2018-04-15 06:02:44 -0400

    Add existing file

--- Problems 1-25/p24.java
+++ Problems 1-25/p24.java
@@ -13,46 +13,79 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 
 */
 
+import java.util.*;
+
+
 public class p24 {
 
 	public static void main(String[] args) {
 
-		int digits = 3;
+		int digits = 4;
 
-		int[] arr = new int[digits];
-
-		for (int i = 0; i < digits; i++) {
-
-			arr[i] = i;
-		}
-
-		int[][] p = new int[factorial(arr.length)][digits];
-
-		for (int i = 0, k = 0; i < p.length; i++) {
-			for (int j = 0; j < p[i].length; j++, k++) {
-
-				p[i][j] = k;
-			}
-			p[i] = term(p[i], i);
-		}
-
+		permutation(digits,factorial(digits));
 	}
 
-	public static int[] term(int[] a, int i) {
+	public static void permutation(int n, int range) {
 
-		if (i == 0) {
-			return a;
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = i + 1;
 		}
-		else {
 
-			int[] arr = a;
-			int count = 0;
+		int count, i, j, temp, index;
+		count = 0;
+		i = arr.length - 1;
+		j = i - 1;
 
-			while (count <= i) {
+		String[] str = new String[range];
 
-				for (int i = 0; i < arr.length; i++)
+		while (count < range) {
+
+			if (j < 0) {
+
+				i = arr.length - 1;
+				j = i - 1;
+
+				temp = arr[i];
+				arr[i] = arr[0];
+				arr[0] = temp;
 			}
+			else {
+
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+
+				i--;
+				j--;
+			}
+
+			String concat = "";
+
+			for (int k = 0; k < arr.length; k++) {
+				concat = concat + Integer.toString(arr[k]);
+			}
+			str[count] = concat;
+
+			count++;
 		}
+
+		int[] list = new int[str.length];
+
+		for (int k = 0; k < str.length; k++) {
+			
+			list[k] = Integer.valueOf(str[k]);
+			System.out.println(list[k] + " term: " + (k + 1));
+		}
+
+		Arrays.sort(list);
+
+		for (int k = 0; k < list.length; k++) {
+			//System.out.println(list[k] + " term: " + (k + 1));
+		}
+
+		//return str;
 	} 
 
 	public static int factorial (int a) {
