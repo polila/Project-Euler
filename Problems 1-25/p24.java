399ca9e2991f37cc9899319e3b1b6d7df838e328
Patrick Olila <polila@ryerson.ca>
2018-04-16 06:26:33 -0400

    Problem 24 LOL

--- Problems 1-25/p24.java
+++ Problems 1-25/p24.java
@@ -20,82 +20,52 @@ public class p24 {
 
 	public static void main(String[] args) {
 
-		int digits = 10;
-		
-		permutation(digits,factorial(digits));
-		
+		int digits = 4;
+
+		int[] arr = new int[digits];
+
+		for (int i = 0; i < arr.length; i++) {
+			
+			arr[i] = i;
+		}
+
+		permutation(arr);
 	}
 
-	public static void permutation(int n, int range) {
+	public static void permutation(int[] list) {
 
-		int[] arr = new int[n];
-		int[] duplicate = new int[n];
+		int[] arr = list;
 
-		for (int i = 0; i < n; i++) {
-			arr[i] = i;
-			duplicate[i] = i;
-		}
+		for (int count = 0; count < factorial(list.length); count++) {
 
-		int count, i, j, temp, index;
-		count = 0;
-		i = arr.length - 1;
-		j = i - 1;
+			String str = "";
 
-		String[] str = new String[range];
+			for (int n = 0; n < list.length; n++) {
 
-		index = 0;
+				str = str + list[n];
+			}
 
-		while (count < range) {
+			System.out.println(str);
 
-			int a = 0;
+			int i, j, k;
+			i = arr.length - 1;
+			j = i - 1;
 
-			while (a < factorial(arr.length - 1)) {
+			while (j >= 1) {
 
-				if (j == 0) {
-					i = arr.length - 1;
-					j = i - 1;
-				}
-
-				String concat = "";
-
-				for (int m = 0; m < arr.length; m++) {
-					concat = concat + Integer.toString(arr[m]);
-				}
-
-				str[count] = concat;
-
-				temp = arr[i];
+				int k = arr[i];
 				arr[i] = arr[j];
-				arr[j] = temp;
-
+				arr[j] = k;
+			
 				i--;
 				j--;
-				a++;
-				count++;
 			}
-
-			index++;
-			if (index > arr.length - 1) {
-				break;
-			}
-
-			temp = duplicate[index - 1];
-			arr[0] = duplicate[index];
-			arr[index] = temp;
-
 		}
 
-		BigDecimal[] list = new BigDecimal[str.length];
 
-		for (int p = 0; p < str.length; p++) {
-			list[i] = new BigDecimal(str[p]);
-			System.out.println(list[i]);
-		}
+	}
 
-		//return str;
-	} 
-
-	public static int factorial (int a) {
+	public static int factorial(int a) {
 
 		int product = 1;
 
