e63e52a3d2c74e0ddb192d688a015f3cf92a4b0d
Patrick Olila <polila@ryerson.ca>
2018-04-18 04:32:38 -0400

    Problem 24

--- Problems 1-25/p24.java
+++ Problems 1-25/p24.java
@@ -20,20 +20,91 @@ public class p24 {
 
 	public static void main(String[] args) {
 
-		int digits = 3;
+		int digits = 10;
 
-		int[] arr = new int[digits];
+		String[] arr = new String[digits];
 
 		for (int i = 0; i < digits; i++) {
-			
-			arr[i] = i;
+
+			arr[i] = Integer.toString(i);
 		}
 
-		permutation(arr, digits);
-	}
-	public static void permutation(int[] arr, int n) {
+		ArrayList<String> permutationList = new ArrayList<String>();
 
-		// Work in progress
+		permutation(arr, arr.length, permutationList);
+
+		String[] p = new String[factorial(digits)];
+		
+		int index = 0;
+		for (String str:permutationList) {
+
+			p[index] = str;
+			System.out.println(p[index] + " | unordered term: " + index);
+			index++;
+		}
+
+		String temp;
+		int count = 0;
+		for (int i = 0; i < p.length - 1; i++) {
+
+			for (int j = i + 1; j < p.length; j++) {
+
+				System.out.print(p[i] + " ");
+
+				if (p[i].compareTo(p[j]) > 0) {
+					System.out.println(">" + p[j] + " | count:" + count);
+					temp = p[i];
+					p[i] = p[j];
+					p[j] = temp;
+				}
+				else {
+					System.out.println("< " + p[j] + " | count:" + count);
+				}
+				
+				count++;
+			}
+
+		}
+
+		for (int i = 0; i < p.length; i++) {
+			System.out.println(p[i]);
+		}
+
+	}
+	// Reference: https://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+	public static void permutation(String[] arr, int n, List<String> numbers) {
+
+		if (n == 1) {
+
+			String concat = "";
+
+			for (int i = 0; i < arr.length; i++) {
+
+				concat = concat + arr[i];
+			}
+
+			System.out.println(concat + " | counting");
+
+			numbers.add(concat);
+
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+
+			swap(arr, i, n - 1);
+
+			permutation(arr, n - 1, numbers);
+
+			swap(arr, i, n - 1);
+		}
+	}
+	// Reference: https://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+	public static void swap(String[] arr, int i, int j) {
+		
+		String temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 
 	public static int factorial(int a) {
